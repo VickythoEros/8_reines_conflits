a023b72c96c29a923f26676f4a6bacf873fff247
VickythoEros <vakaramokokone@gmail.com>
2021-11-26 12:26:06 +0100

    changement de la forme des tables

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # MY IMPORTS
 import numpy as np
 import random as rd
+
 # MY FUNCTIONS
 def conflits(T):
     """
@@ -10,17 +11,18 @@ def conflits(T):
     :return: S
     """
     S = 0
-    for i in T:
+    n = len(T)
+    for i in range(n):
         m = 0
-        for j in T:
-            if i[0] == j[0] or i[1] == j[1] or abs(i[0] - j[0]) == abs(i[1] - j[1]):
+        for j in range(n):
+            if T[i] == T[j] or abs(i - j) == abs(T[i] - T[j]):
                 m +=1
         S += m -1
-        #print(f" {i} : {m-1}")
+        print(f" {i} : {m-1}")
     return S
 
 
-def regeneration_s(T,n,l,c):
+def regeneration_s(T,l,c):
     """
     fonction permettant d'effectuer un decalage de 'l' ligne et 'c' colonne
     :param T: liste des positions des 8 reines
@@ -29,22 +31,27 @@ def regeneration_s(T,n,l,c):
     :param c: le nombre de decalage en collone
     :return: new_T
     """
-    new_T = []
-    for i in T:
-        li = (i[0] + l)%(n - 1) if (i[0] + l) > (n-1) else i[0] + l
-        co = (i[1] + c)%(n - 1) if (i[1] + c) > (n-1) else i[1] + c
-        new_T.append([li,co])
+    n = len(T)
+    new_T = [0 for i in range(n)]
+
+    for i in range(n):
+        li = (i + l)%(n - 1) if (i + l) > (n-1) else i + l
+        co = (T[i] + c)%(n - 1) if (T[i] + c) > (n-1) else T[i] + c
+
+        new_T[li] = co
+
     return new_T
 
 
 def _initialize(n):
-     return [ [rd.randint(0,n-1),rd.randint(0,n-1)] for i in range(n) ]
+
+     return [ rd.randint(0,n-1) for i in range(n) ]
 
 
 
 def _show_queens_in_echec(matrice,T):
-    for i in T:
-        matrice[i[0], i[1]] = 1
+    for i in range(len(T)):
+        matrice[i, T[i]] = 1
     return matrice
 
 
@@ -55,13 +62,15 @@ if __name__ == '__main__':
     echequier = np.zeros((n,n)) # creation d'une matrice nxn remplie de zeros
     is_ok = False
 
+    # initialisation
+    #T = [7, 0, 1, 3, 1, 5, 2, 4]
     T = _initialize(n)
-    #T = [[1, 1], [4, 0], [6, 6], [0, 1], [2, 3], [1, 0], [5, 6], [4, 6]]
+    print(T)
     s = conflits(T)
     tables_values.append(s)
 
     while is_ok == False:
-        T1 = regeneration_s(T,n,1,2)
+        T1 = regeneration_s(T,1,2)
         print(T1)
         s1 = conflits(T1)
         if s1 > tables_values[-1] :
